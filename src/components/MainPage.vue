ce10174a1c83acab685ad66b48ac0b4b964b10d8
ivanLFN <LFN.current@yandex.com>
2023-10-21 04:18:33 -0400

    adding input for stantion enter

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,25 +1,47 @@
 <template>
   <div>
     <div class="home-el">
-      <div class="imgage-info">
+      <div class="image-info" :class="{ 'hidden': !showImageInfo }">
         <img src="/img/info-icon.png" alt="info-icon">
         <div style="margin-top: 40px;">Нет действующей</div>
         <div style="margin-top: -10px;">сессии</div>
         <div style="margin-top: 20px;">Начать?</div>
       </div>
+      <div class="new-session-info pt-5" v-if="!showImageInfo">
+        <img src="/img/enter-code.png" style="width: 4rem;" alt="info-icon">
+        <div style="margin-top: 30px; margin-bottom: 30px;">Введите номер станции</div>
+        <div class="enter-block col-12 d-flex justify-content-center align-items-center">
+          <div class="col-10">
+            <div class="input-group mb-3 enter-code">
+              <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="button-addon2" v-model="numericValue" @input="restrictInput">
+              <button class="btn btn-outline-secondary button-style" type="button" id="button-addon2">
+                <img src="/img/arrow-input.png" alt="info-icon">
+              </button>
+            </div>
+          </div>  
+        </div>
+
+        
+        
+      </div>
     </div>
     <div class="new-session">
-      <a href="#" class="img-new-session">
+      <a href="#" class="img-new-session" @click="toggleImageInfo">
         <img src="/img/logo_small.png" alt="logo-link" class="img-settings"/>
       </a>
     </div>
     <div class="bar">
       <row>
         <div class="home bottom-bar d-flex justify-content-center align-items-center" style="left: 0; border-radius: 0 100px 0 0;">
-          <img src="/img/home-icon.png" alt="logo-link" class="img-settings" style="width: 3.3rem;"/>
+          <a href="#">
+            <img src="/img/home-icon.png" alt="logo-link" class="img-settings" style="width: 3.3rem;"/>
+          </a>
+          
         </div>
         <div class="menu bottom-bar d-flex justify-content-center align-items-center" style="right: 0; border-radius: 100px 0 0 0;">
-          <img src="/img/menu-icon.png" alt="logo-link" class="img-settings" style="width: 3rem;"/>
+          <a>
+            <img src="/img/menu-icon.png" alt="logo-link" class="img-settings" style="width: 3rem;"/>
+          </a>
         </div>
       </row>
     </div>
@@ -28,8 +50,21 @@
 
 <script>
 export default {
-  name: 'MainPage'
-  
+  name: 'MainPage',
+  data() {
+    return {
+      showImageInfo: true,
+      numericValue: '',
+    };
+  }, 
+  methods: {
+    toggleImageInfo() {
+      this.showImageInfo = !this.showImageInfo;
+    },
+    restrictInput() {
+      this.numericValue = this.numericValue.replace(/[^0-9]/g, '');
+    }
+  }
 }
 </script>
 
@@ -66,10 +101,30 @@ export default {
   transform: translate(-50%, -50%);
 }
 
-.imgage-info {
+.image-info {
   margin-top: 60px;
   margin-bottom: 80px;
   font-size: 1.9rem;
+}
+
+.new-session-info {
+  font-size: 1.9rem;
+}
+
+.hidden {
+  display: none;
+}
+
+.enter-code {
+  height: 40px;
+}
+
+.button-style {
+  background-color: #1fc4c2;
+}
+
+.form-control {
+  font-size: 2rem;
 }
 </style>
   
