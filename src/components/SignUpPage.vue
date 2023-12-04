40bd3dab44dfa33a096fe9523cbe3f6fad973e2a
ivanLFN <LFN.current@yandex.com>
2023-12-03 19:43:17 -0800

    adding sign-up page frontend

---
+++ src/components/SignUpPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="signup-form">
+      <SignUpForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import SignUpForm from '@/components/SignUpForm.vue'
+export default {
+  name: 'SignUpPage',
+  components: {
+    SignUpForm
+  }
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+</style>
